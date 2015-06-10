2987a46bffbf6c82c42e5369d336da8d8e8f259c
John Regner <john@johnregner.com>
2015-06-10 00:26:27 -0700

    Added getting sequence number

--- BitPacking.playground/Pages/Getting Sequence Numbers.xcplaygroundpage/Contents.swift
+++ BitPacking.playground/Pages/Getting Sequence Numbers.xcplaygroundpage/Contents.swift
@@ -4,18 +4,43 @@ import Foundation
 
 let path = NSBundle.mainBundle().pathForResource("input", ofType: "dat")
 let data = readDataFromFile(path)
-
 let newData = removeNumberOfBytes(2, fromData: data) // remove the 2 unused bytes
 
 /*:
 The next thing in the data format is the Sequence Number.
 
-The sequence number is in the next two bytes. Lets grab the next two bytes and save them then drop the 2 bytes from the data. 
+The sequence number is in the next two bytes. Lets grab the next two bytes and save them.
 
-    func getSequenceNumberFromData(data: NSData?) -> (NSData, Int)
+    func getSequenceNumberFromData(data: NSData?) -> Int
 
 */
 
+func getSequenceNumberFromData(data: NSData?) -> Int?{
+    
+/*:
+Since the data is an Optional we need to check it for nil.
+*/
+    switch data {
+    case .Some(let data):
+        
+/*:
+Now that we have the data we can use `NSData.getBytes()` to read 2 bytes from the data.
+The signature for `getBytes()` is a bit scary. But we can figure it out. 
 
+    func getBytes(_ buffer: UnsafeMutablePointer<Void>, length length: Int)
 
+We need a pointer so we use the `&` operator. The pointer must be mutable so we declare it as a var. The void is there to specify that the type system does not know what type of data we are going to get out of the data. But we know that it is going to be a 16 bit integer. We need to initialize the seq variable to zero so the compiler can reserver and zero out some memory for us.
+*/
+        var seq: UInt16 = 0
+        data.getBytes(&seq, length: 2)
+        return Int(seq.byteSwapped)
+/*:
+Finally just return nil if the data was not there
+*/
+    case .None: return nil
+    }
+
+}
+
+let seq = getSequenceNumberFromData(newData)
 //: [Next](@next)
